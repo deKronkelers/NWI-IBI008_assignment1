65268af63959ec0750675099d7d66f17c80b0fcc
Hendrik Werner <hendrik.to@gmail.com>
2016-09-04 17:46:48 +0200

    Use correct array constructor.

--- answers/assignment1-Hendrik_Werner_s4549775-Constantin_Blach_s4329872.py
+++ answers/assignment1-Hendrik_Werner_s4549775-Constantin_Blach_s4329872.py
@@ -1,22 +1,22 @@
 # author: Hendrik Werner // s4549775
 # author: Constantin Blach // s4329872
 import numpy
-from numpy import matrix
+from numpy import array
 
 # assignment 1.1.1
-x = matrix("6 7 8 9 10 11 12")
+x = array([6, 7, 8, 9, 10, 11, 12])
 print("x = {}".format(x))
 
-y = matrix("3 7 11 15 19 23 27")
+y = array([3, 7, 11, 15, 19, 23, 27,])
 print("y = {}".format(y))
 
-w = matrix("1 1 0 0.5 1 1.5 2 0 0")
+w = array([1, 1, 0, 0.5, 1, 1.5, 2, 0, 0])
 print("w = {}".format(w))
 
-s = matrix("100 98.8 97.6 96.4 95.2")
+s = array([100, 98.8, 97.6, 96.4, 95.2])
 print("s = {}".format(s))
 
-z = matrix(".7 1.0 1.3 1.6 1.9 2.2 2.5 2.8")
+z = array([.7, 1.0, 1.3, 1.6, 1.9, 2.2, 2.5, 2.8])
 print("z = {}".format(z))
 
 # assignment 1.1.1 a
