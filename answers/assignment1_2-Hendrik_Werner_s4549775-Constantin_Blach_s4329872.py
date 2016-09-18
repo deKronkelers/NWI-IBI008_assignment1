59d955f16573ef7da6b79e2e80e2a5c9cd367be6
Hendrikto <hendrik.to@gmail.com>
2016-09-18 20:49:16 +0200

    Read in the matrix from the excel file.

--- answers/assignment1_2-Hendrik_Werner_s4549775-Constantin_Blach_s4329872.py
+++ answers/assignment1_2-Hendrik_Werner_s4549775-Constantin_Blach_s4329872.py
@@ -1,6 +1,18 @@
 # author: Hendrik Werner // s4549775
 # author: Constantin Blach // s4329872
 
+import numpy
+import xlrd
+from numpy import empty
+
 # assignment 1.2.1
+with xlrd.open_workbook(filename="nanonose.xls") as book:
+    sheet = book.sheet_by_index(0)
+    offset_row = 2
+    offset_col = 3
+    data = empty((90, 8))
+    for row in range(90):
+        data[row] = sheet.row_values(rowx=offset_row + row, start_colx=offset_col)
+    print(data)
 
 # assignment 1.2.2
