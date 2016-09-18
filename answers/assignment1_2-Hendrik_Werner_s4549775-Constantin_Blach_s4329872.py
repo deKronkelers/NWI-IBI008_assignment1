f1821cf186ef2215a42ef8e330b27854d084639f
Hendrikto <hendrik.to@gmail.com>
2016-09-19 00:14:23 +0200

    Add answer to 1.2.2 c.

--- answers/assignment1_2-Hendrik_Werner_s4549775-Constantin_Blach_s4329872.py
+++ answers/assignment1_2-Hendrik_Werner_s4549775-Constantin_Blach_s4329872.py
@@ -17,3 +17,15 @@ with xlrd.open_workbook(filename="nanonose.xls") as book:
     # assignment 1.2.1 b
     data_plot = scatter(data[:, 0], data[:, 1])
     show()
+
+    # assignment 1.2.2 c
+    means = array([data[:, col].mean() for col in range(8)])
+    Y = data - np.ones((90, 1)) * means
+    U, s, Vt = linalg.svd(Y)
+    sum_of_squares = sum([elem ** 2 for elem in s])
+    pms = [(s[m] ** 2)/ sum_of_squares for m in range(8)]
+    print("Pricipal Components = {}".format(s))
+    print("The first 3 PCs account for {}% of variation.".format(sum(pms[:3]) * 100))
+
+    plot(pms)
+    show()
