13abf5ad19d798172f70b8b6f9fd4450b479c05e
Hendrikto <hendrik.to@gmail.com>
2016-09-18 21:23:52 +0200

    Add answer to 1.2.1 b.

--- answers/assignment1_2-Hendrik_Werner_s4549775-Constantin_Blach_s4329872.py
+++ answers/assignment1_2-Hendrik_Werner_s4549775-Constantin_Blach_s4329872.py
@@ -1,9 +1,8 @@
 # author: Hendrik Werner // s4549775
 # author: Constantin Blach // s4329872
 
-import numpy
 import xlrd
-from numpy import empty
+from pylab import *
 
 # assignment 1.2.1
 with xlrd.open_workbook(filename="nanonose.xls") as book:
@@ -15,4 +14,6 @@ with xlrd.open_workbook(filename="nanonose.xls") as book:
         data[row] = sheet.row_values(rowx=offset_row + row, start_colx=offset_col)
     print(data)
 
-# assignment 1.2.2
+    # assignment 1.2.2
+    data_plot = scatter(data[:, 0], data[:, 1])
+    show()
