215b062ecc7ea8e7f85dac46cb4d8ee00c397257
Hendrikto <hendrik.to@gmail.com>
2016-09-19 01:15:42 +0200

    Add code for 1.3.2.

--- answers/assignment1_3-Hendrik_Werner_s4549775-Constantin_Blach_s4329872.py
+++ answers/assignment1_3-Hendrik_Werner_s4549775-Constantin_Blach_s4329872.py
@@ -1,6 +1,14 @@
 # author: Hendrik Werner // s4549775
 # author: Constantin Blach // s4329872
 
+import numpy
+import scipy
+
 # assignment 1.3.1
 
 # assignment 1.3.2
+v1 = numpy.array([14, 22, 13, 44, 25])
+v2 = numpy.array([41, 5, 1, 3, 5])
+
+print(scipy.spatial.distance.cosine(5 * v1, v2)
+      == scipy.spatial.distance.cosine(v1, v2))
